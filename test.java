16b348c867cf26b0bcd59b0b4bdd1065f2e7f9ec
ronglums <banyanrongrong@gmail.com>
2021-04-22 10:53:33 -0700

    updates

--- test.java
+++
@@ -1,8 +0,0 @@
-public class Test {
-    public static void main(String args[]) {
-        String array[] = {"Hello, World", "Hi there, Everyone", "6"};
-        for (String i : array) {
-          System.out.println(i);
-        }
-    }
-}
--- try.py
+++ try.py
@@ -1,7 +1,15 @@
-stuff = ["Hello, World!", "Hi there, Everyone!", 6]
-for i in stuff:
-    print(i)
+def get_symbols(file_name):
+    with open(file_name, "r") as in_file:
+        records = []
+        count = 0
+        symbol_set = ""
+        for line in in_file:
+            symbol_set = symbol_set + line[:-1] + ','
+            count = count + 1
+            if count % 50 == 0:
+                records.append(symbol_set)
+                symbol_set = ""
 
-
-
+        records.append(symbol_set)
+        return records
 
